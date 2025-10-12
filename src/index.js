730fb5af1b317a60cad374d6e888c0f43416055f
Md. Touhidur Rahman <touhidurrr@pm.me>
2025-10-13 01:36:04 +0600

    enable cors

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const api = require("./routes/api");
 
 const app = express();
 
+app.use(cors({ origin: true, credentials: true }));
 app.use(api);
 
 const host = process.env.HOST ?? "0.0.0.0";
