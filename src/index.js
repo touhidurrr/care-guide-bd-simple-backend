955e2c4f391622fa991fe553c399e89ab5d5f25d
Md. Touhidur Rahman <touhidurrr@pm.me>
2025-12-20 07:02:08 +0600

    unix socket support and dependencies update

--- src/index.js
+++ src/index.js
@@ -10,6 +10,18 @@ app.use(api);
 
 const host = process.env.HOST ?? "0.0.0.0";
 const port = process.env.PORT ?? 4041;
-app.listen({ host, port }, () =>
-  console.log(`Server running at http://${host}:${port}`),
-);
+const unix = process.env.UNIX_SOCKET_PATH;
+
+const server = app.listen(unix ? unix : { host, port });
+
+server.once("listening", () => {
+  const serverAddress = server.address();
+
+  if (typeof serverAddress === "string") {
+    return console.log(`Server listening to unix socket ${serverAddress}`);
+  }
+
+  const { address, port, family } = serverAddress;
+  const hostname = family === "IPv6" ? `[${address}]` : address;
+  console.log(`Server listening to http://${hostname}:${port}`);
+});
