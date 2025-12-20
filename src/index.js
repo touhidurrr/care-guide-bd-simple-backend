282896297f74ad98881ee7507e172c21188a01aa
Md. Touhidur Rahman <touhidurrr@pm.me>
2025-12-20 07:29:36 +0600

    fix previously implemented unix sockets permissions issue

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const api = require("./routes/api");
+const { existsSync, unlinkSync, chmodSync } = require("fs");
 
 const app = express();
 
@@ -12,12 +13,17 @@ const host = process.env.HOST ?? "0.0.0.0";
 const port = process.env.PORT ?? 4041;
 const unix = process.env.UNIX_SOCKET_PATH;
 
+if (unix && existsSync(unix)) {
+  unlinkSync(unix);
+}
+
 const server = app.listen(unix ? unix : { host, port });
 
 server.once("listening", () => {
   const serverAddress = server.address();
 
   if (typeof serverAddress === "string") {
+    chmodSync(unix, 0o777);
     return console.log(`Server listening to unix socket ${serverAddress}`);
   }
 
